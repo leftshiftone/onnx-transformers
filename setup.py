ae2ae058bf15a3b7ef37bb9c0a8a8d02361cc64f
Thomas Kloiber <thomas.kloiber@leftshift.one>
2025-06-11 13:04:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 # read the contents of your README file
 from pathlib import Path
 
@@ -8,18 +9,18 @@ from setuptools import find_packages, setup
 long_description = Path(__file__).with_name("README.md").read_text()
 
 setup(
-    name='onnx_transformers',
+    name="onnx_transformers",
     packages=find_packages(exclude=("test")),
-    version='1.1.1',
-    license='Apache Software License',
-    description='tbd',
+    version="1.2.0",
+    license="Apache Software License",
+    description="tbd",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='c7nw3r',
-    url='https://github.com/leftshiftone/onnx_transformers',
-    download_url='https://github.com/leftshiftone/onnx_transformers/archive/refs/tags/v1.1.0.tar.gz',
-    keywords=['onnx', 'transformers'],
-    setup_requires=['setuptools_scm'],
+    author="c7nw3r",
+    url="https://github.com/leftshiftone/onnx_transformers",
+    download_url="https://github.com/leftshiftone/onnx_transformers/archive/refs/tags/v1.2.0.tar.gz",
+    keywords=["onnx", "transformers"],
+    setup_requires=["setuptools_scm"],
     include_package_data=True,
     install_requires=[
         str(r)
@@ -28,9 +29,9 @@ setup(
         )
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
 )
