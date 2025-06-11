a2de09dd084edb4361f82e09f5e3f18f878e948b
Thomas Kloiber <thomas.kloiber@leftshift.one>
2025-06-11 13:04:26 +0200

    Update dependencies

--- test/model_for_encoding_test.py
+++ test/model_for_encoding_test.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from unittest import TestCase
 
 from onnx_transformers.model_for_encoding import ModelForEncoding
 
 
 class ModelForEncodingTest(TestCase):
-
     def test_abc(self):
-        encoder = ModelForEncoding("./dummy")
+        path = Path(__file__).parent / "dummy"
+        encoder = ModelForEncoding(str(path))
         encoder("This is an example text")
